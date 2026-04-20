e8808d647566fcca5b119c8f9798d61428dc7405
WANG-YongLE <a2a45a789@gmail.com>
2026-04-20 23:36:42 +0800

    A

--- Assets/script/BallMove.cs
+++ Assets/script/BallMove.cs
@@ -2,9 +2,9 @@
 
 [RequireComponent(typeof(Rigidbody), typeof(SphereCollider))]
 public class BallMove : MonoBehaviour {
-    public Vector3 velocity = new Vector3(2f, 2f, 2f);
-    public float maxSpeed = 5f;
-
+    public Vector3 velocity = new Vector3(0.001f, 0.001f, 0.001f); 
+    public float maxSpeed = 5f; 
+    public float acceleration = 0.04f; 
     Rigidbody rb;
     float radius;
 
@@ -19,8 +19,11 @@ public class BallMove : MonoBehaviour {
     void FixedUpdate() {
         float dt = Time.fixedDeltaTime;
 
-        if (velocity.magnitude > maxSpeed)
-            velocity = velocity.normalized * maxSpeed;
+      
+        float speed = velocity.magnitude;
+        speed += acceleration * dt;
+        speed = Mathf.Min(speed, maxSpeed); 
+        velocity = velocity.normalized * speed;
 
         Vector3 dir = velocity.normalized;
         float dist = velocity.magnitude * dt;
@@ -29,28 +32,33 @@ public class BallMove : MonoBehaviour {
 
         if (Physics.SphereCast(rb.position, radius, dir, out hit, dist)) {
 
-      
             float safeDist = Mathf.Max(hit.distance - 0.002f, 0f);
             rb.MovePosition(rb.position + dir * safeDist);
 
-       
+            //處理碰撞
+            if (hit.collider.CompareTag("TopBoard")) {
+                ScoreManager.Instance.AddScore_1(1);
+            } else if (hit.collider.CompareTag("DownBoard")) {
+                ScoreManager.Instance.AddScore_2(1);
+            } 
+            //else if (!hit.collider.CompareTag("Wall")) {
+               // Time.timeScale = 0f;
+          //  }
+
+            //  反射
             velocity = Vector3.Reflect(velocity, hit.normal);
 
         } else {
             rb.MovePosition(rb.position + velocity * dt);
+        
         }
     }
-    void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("TopBoard")) {
-            ScoreManager.Instance.AddScore_1(1);
-        }
-        else if (collision.gameObject.CompareTag("DownBoard")) {
-            ScoreManager.Instance.AddScore_2(1);
-        }
-        else if(collision.gameObject.CompareTag("Wall")) {
-            return;
-        }
-        else { Time.timeScale = 0f; }
+    float GetMaxSpeedByTime(float t) {
+        if (t < 10f) return 0.01f;
+        else if (t < 20f) return 0.1f;
+        else if (t < 30f) return 0.4f;
+        else if (t < 40f) return 1f;
+        else if (t < 50f) return 2f;
+        else return 5f;
     }
-
 }
